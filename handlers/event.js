fb9057aa2b236ca114edae8529fc141b334b0632
LelaBenet <41063357+LelaBenet@users.noreply.github.com>
2020-01-13 12:35:54 +0200

    Handel files
    Event and Command handler

---
+++ handlers/event.js
@@ -0,0 +1,15 @@
+// Handles event exacution.
+
+const { readdirSync } = require('fs');
+
+module.exports = async bot => {
+  const load = dirs => {    
+    const events = readdirSync(`./events/${dirs}/`).filter(d => d.endsWith('.js'));
+    for (let file of events) {
+      const evt = require(`./../events/${dirs}/${file}`);
+      let eName = file.split('.')[0];
+      bot.on(eName, evt.bind(null, bot));
+    }
+  };
+  ['client', 'guild', 'logger'].forEach(x => load(x));
+};
